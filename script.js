1e6fa174ca73799069e3c469bd1697d60f1ad3a3
RoqiaAli <111224604+RoqiaAli@users.noreply.github.com>
2022-09-16 00:48:20 +0300

    the converts

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ x=10;
 y=12;
 z=x+y;
 
-console.count(z);
+console.log(z);
 
-// teacher really i'm so sorry cause the last day i was taird so i couldn;t foucs with you :(
+// this first h.w
+
+// teacher really i'm so sorry because the last day i was taird so i couldn;t foucs with you :(
+
+// the sconed h.w
+
+
+let C;
+let F;
+
+//to convert i'll get example c=20 
+
+// F to C
+
+function convert(){
+    const fahrenheit  = parent(
+        document.getElementById("fahrenheit").value
+    );
+
+    const C =(fahrenheit-32)*0.5556;
+    document.getElementById("C").innerText =celsius;
+}
