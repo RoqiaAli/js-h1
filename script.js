7597e9a25ed569a3cfaa7e436617d94591132c61
RoqiaAli <111224604+RoqiaAli@users.noreply.github.com>
2022-09-16 11:44:13 +0300

    convert Fahrenheit & Celsius

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// this first h
+
 let x;
 let y;
 let z;
@@ -6,27 +8,34 @@ x=10;
 y=12;
 z=x+y;
 
+console.log(x);
+console.log(y);
 console.log(z);
 
-// this first h.w
+
 
 // teacher really i'm so sorry because the last day i was taird so i couldn;t foucs with you :(
 
 // the sconed h.w
 
-
-let C;
-let F;
-
-//to convert i'll get example c=20 
-
 // F to C
 
 function convert(){
-    const fahrenheit  = parent(
+    const fahrenheit  = parseInt(
         document.getElementById("fahrenheit").value
     );
 
-    const C =(fahrenheit-32)*0.5556;
-    document.getElementById("C").innerText =celsius;
+    const C =(fahrenheit-32) *0.5556;
+    document.getElementById("C").innerText =C;
+}
+
+
+// C to F
+function convert2(){
+    const celsius  = parseInt(
+        document.getElementById("celsius").value
+    );
+
+    const F =( celsius / 0.5556 )  +32;
+    document.getElementById("F").innerText =F;
 }
